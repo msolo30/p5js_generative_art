14bcea30f69634d55f7315f0558f9be15179f33b
Sophia <sophiam2702@tuckahoeschools.org>
2025-03-06 13:31:40 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// Flower object.
+let flower = { 
+    x: 200, 
+    y: 100,
+    emoji: '🌸' 
+  };
+
+  function setup() {
+    createCanvas(400, 400);
+  
+    // Print object in console.
+    console.log(flower);
+  }
+  function draw() {
+    background("lightblue");
+  }
+
+  // Define an object
+let objectName = {
+    property1: value1,
+    property2: value2,
+    property3: value3
+  };
